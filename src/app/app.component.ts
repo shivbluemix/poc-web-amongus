cf91b8d45b281a8a9c12050b2d494b26f6c91a9c
vIvek sIngh <vivek.singh6@cbre.com>
2025-03-27 09:15:00 +0530

    'initial'

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+import { Message } from './models/message.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  message: string = '';
+  output: string | null = null;
+
+  constructor(private apiService: ApiService) {}
+
+  onSubmit() {
+    this.apiService.sendMessage(this.message).subscribe((response: Message) => {
+      this.output = response.content; 
+    }, error => {
+      this.output = 'Error fetching data';
+    });
+  }
+}
